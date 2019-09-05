19d653f3cca7ee899cbaf5f8f7901692e507e0d1
Quinlor <stefan.leng@gmx.de>
2019-09-05 23:16:12 +0200

    gamma correction

--- src/Aardvark_test/Shader.fs
+++ src/Aardvark_test/Shader.fs
@@ -32,6 +32,9 @@ module Lighting =
 
     let internal lighting (twoSided : bool) (v : Vertex) =
         fragment {
+            let gamma  = 2.2
+            let c = pow (v.c.XYZ) (V3d(gamma))
+            //let c = v.c.XYZ
             let light = uniform.Light
             let (ld, lc)  = 
                 match  light  with
@@ -40,7 +43,7 @@ module Lighting =
             let n = v.n |> Vec.normalize
             let h = ld
 
-            let ambient = 0.1
+            let ambient = 0.2
             
             let diffuse = 
                 Vec.dot ld n 
@@ -49,9 +52,10 @@ module Lighting =
             let l = ambient + (1.0 - ambient) * diffuse
 
             let spec = V3d.III
-            let s = Vec.dot h n 
+            let s = Vec.dot h n |> if twoSided then abs else max 0.0
 
-            return V4d(v.c.XYZ * l * lc + spec * (pown s 32) * lc, v.c.W)
+            return pow (V4d(c * l * lc + spec * (pown s 32) * lc, v.c.W)) (V4d(1.0/gamma))
+            //return V4d(c * l * lc + spec * (pown s 32) * lc, v.c.W)
         }
 
 module SLESurfaces = 
